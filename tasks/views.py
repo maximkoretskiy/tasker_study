e7412bc448ff27c5f9ec68b83d2e74121b3b8e7a
remedy <mr.green.tv@gmail.com>
2013-03-20 23:38:28 +0400

    Task adding implemented

--- tasks/views.py
+++ tasks/views.py
@@ -1,27 +1,31 @@
 # Create your views here.
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 from models import Task, TaskAddForm
 
 @login_required
 def dash(request):
-    form  = TaskAddForm()
     week_list = Task.objects.filter(executor_id=request.user.id, level='W')
     month_list = Task.objects.filter(executor_id=request.user.id, level='M')
     quoter_list = Task.objects.filter(executor_id=request.user.id, level='Q')
     return render_to_response('tasks/dash.html', locals() )
 
 def task_add(request):
-    if request.method == 'GET':
-        data = request.GET.copy()
-        data.appendlist('executor', request.user.id)
-        data.appendlist('status', 'C')
-        form = TaskAddForm(data)
+    if request.method == 'POST':
+        form = TaskAddForm(request.POST)
         if form.is_valid():
-            print 'saving data'
-            form.save()
+            task = form.save(commit=False)
+            task.executor = request.user
+            task.save()
+            return HttpResponseRedirect(reverse('dashboard'))
+
         else:
             pass
         #assert False
-        return render_to_response('tasks/dash.html', RequestContext(request ,locals()) )
+    else:
+        form = TaskAddForm()
+
+    return render_to_response('tasks/add_task.html', RequestContext(request ,locals()) )
