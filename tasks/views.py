96ec20316ebfa0e76da624fea59b22fdea6165ef
remedy <mr.green.tv@gmail.com>
2013-03-28 01:08:17 +0400

    Added login_required decorator on all task views

--- tasks/views.py
+++ tasks/views.py
@@ -7,6 +7,8 @@ from django.contrib.auth.decorators import login_required
 from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic.list import ListView
 from models import Task, Project, TaskAddForm, TaskEditForm
+from tasker.common import class_view_decorator
+
 
 @login_required
 def dash(request):
@@ -16,12 +18,14 @@ def dash(request):
     quoter_list = Task.objects.filter(executor_id=request.user.id, level='Q')
     return render_to_response('tasks/dash.html', locals() )
 
+@login_required
 def task_delete(request, task_id):
     task = get_object_or_404(Task,pk=task_id)
     task.status = 'A'
     task.save()
     return HttpResponseRedirect(reverse('dashboard'))
 
+@login_required
 def task_edit(request, task_id):
     task = get_object_or_404(Task,pk=task_id)
     page_title = u"Редактирование задачи %s" % task.title
@@ -35,12 +39,13 @@ def task_edit(request, task_id):
 
     return render_to_response('tasks/add_task.html', RequestContext(request ,locals()) )
 
+@login_required
 def task_status(request, task_id, status):
     task = get_object_or_404(Task,pk=task_id)
     task.complete(status)
     return HttpResponseRedirect(reverse('dashboard'))
 
-
+@login_required
 def task_add(request):
     page_title = "Создание задачи"
     if request.method == 'POST':
@@ -56,28 +61,35 @@ def task_add(request):
     return render_to_response('tasks/add_task.html', RequestContext(request ,locals()) )
 
 
+@login_required
 def _change_context(parent,**kwargs):
     context = super(parent.__class__, parent).get_context_data(**kwargs)
     context['page_title'] = parent.page_title
     return context
 
+@class_view_decorator(login_required)
 class ProjectList(ListView):
     model = Project
     page_title = "Список проектов"
     get_context_data = _change_context
 
+@class_view_decorator(login_required)
 class ProjectCreate(CreateView):
     model = Project
     success_url = reverse_lazy('project_list')
     page_title = "Создание проекта"
     get_context_data = _change_context
 
+
+@class_view_decorator(login_required)
 class ProjectUpdate(UpdateView):
     model = Project
     success_url = reverse_lazy('project_list')
     page_title = "Редактирование проекта"
     get_context_data = _change_context
 
+
+@login_required
 def project_delete(request, pk):
     project = get_object_or_404(Project, pk=pk)
     project.delete()
